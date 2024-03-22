7a40426a03cd8f40891d84093616efdf309ea657
MahmoudHesham1350 <mahmoudhesham656@gmail.com>
2024-03-23 00:59:29 +0200

    made invert color

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include "Image_Class.h"
+using namespace std;
+
+class EditImage: private Image {
+
+private:
+
+public:
+    Image invert_color(Image main_image) {
+        Image res_image(main_image.width, main_image.height);
+        for (int wedith = 0; wedith < main_image.width; wedith++){
+            for (int height = 0; height < main_image.height; height++) {
+                for (int color = 0; color < 3; color++){
+                 res_image(wedith, height, color) = 255 - main_image(wedith, height, color);
+                }}}
+        return res_image;
+    }
+
+
+};
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    EditImage Editor;
+    Image img("arrow.jpg");
+
+    Image img_inverted = Editor.invert_color(img);
+    img_inverted.saveImage("test.jpg");
+
     return 0;
 }
