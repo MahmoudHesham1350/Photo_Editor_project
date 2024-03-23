6fe63dc8a861d951b52794aa66b09eb620d6ed5e
MahmoudHesham1350 <mahmoudhesham656@gmail.com>
2024-03-23 21:25:15 +0200

    add to the code

--- main.cpp
+++ main.cpp
@@ -4,31 +4,35 @@ using namespace std;
 class EditImage: private Image {
 
 private:
-
+    static void copy_img(Image &img_from, Image &img_to) {
+        for (int width = 0; width < img_from.width; width++){
+            for (int height = 0; height < img_from.height; height++) {
+                for (int color = 0; color < 3; color++){
+                    img_to(width, height, color) = img_from(width, height, color);
+                }}}
+    }
 
 public:
 
     void invert_img(Image &main_img) {
         Image res_img(main_img.width, main_img.height);
-
-        for (int wedith = 0; wedith < main_img.width; wedith++){
+        for (int width = 0; width < main_img.width; width++){
             for (int height = 0; height < main_img.height; height++) {
                 for (int color = 0; color < 3; color++){
-                    main_img(wedith, height, color) = 255 - main_img(wedith, height, color);
+                    main_img(width, height, color) = 255 - main_img(width, height, color);
                 }}}
     }
 
-
-};
+    };
 
 
 
 int main() {
-    EditImage Editor;
+    EditImage Edit;
     Image img("arrow.jpg");
+    Edit.invert_img(img);
 
-    Editor.invert_img(img);
-    img.saveImage("invert.jpg");
+    img.saveImage("result.jpg");
 
     return 0;
 }
