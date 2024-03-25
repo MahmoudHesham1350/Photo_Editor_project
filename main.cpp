728217ff52dcf5c1c62c66641039f844774ce779
MahmoudHesham1350 <mahmoudhesham656@gmail.com>
2024-03-25 17:46:07 +0200

    added the rotation function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "Image_Class.h"
 using namespace std;
 
-class EditImage: private Image {
+class EditImage: protected Image {
 
 private:
     static void copy_img(Image &img_from, Image &img_to) {
@@ -12,27 +12,52 @@ private:
                 }}}
     }
 
+    Image rotate_img_90(Image &main_img) {
+        Image res_img(main_img.height, main_img.width);
+        for(int height = 0; height < main_img.height; height++){
+            for(int width = 0; width < main_img.width; width++){
+                for(int color = 0; color < 3; color++){
+                    res_img(height, width, color) = main_img(width, main_img.height - 1 - height, color);
+                }}}
+        return res_img;
+    }
+
 public:
 
-    void invert_img(Image &main_img) {
+    Image invert_img(Image &main_img) {
         Image res_img(main_img.width, main_img.height);
         for (int width = 0; width < main_img.width; width++){
             for (int height = 0; height < main_img.height; height++) {
                 for (int color = 0; color < 3; color++){
-                    main_img(width, height, color) = 255 - main_img(width, height, color);
+                    res_img(width, height, color) = 255 - main_img(width, height, color);
                 }}}
+        return res_img;
     }
 
-    };
+    Image rotate_img(Image &main_img, int rotation = 90) {
+        if (rotation == 90){
+            return rotate_img_90(main_img);
+        }
+        Image rotated_img_180 = rotate_img_90(main_img);
+        if (rotation == 180){
+            return rotate_img_90(rotated_img_180);
+        }
+        Image rotated_img_270 = rotate_img_90(rotated_img_180);
+        if (rotation == 270){
+            return rotate_img_90(rotated_img_270);
+        }
+    }
+};
 
 
 
 int main() {
     EditImage Edit;
     Image img("arrow.jpg");
-    Edit.invert_img(img);
+    Image res = Edit.rotate_img(img, 270);
 
-    img.saveImage("result.jpg");
+
+    res.saveImage("result.jpg");
 
     return 0;
 }
